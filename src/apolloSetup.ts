994cbf6d0d0b86f10ae3118d00765822dc2ca767
markhachatryan <markhachatryan111@gmail.com>
2019-02-21 18:40:45 +0400

    token

--- src/apolloSetup.ts
+++ src/apolloSetup.ts
@@ -5,7 +5,7 @@ import ApolloClient from "apollo-client";
 const cache = new InMemoryCache();
 console.log(process.env);
 const GITHUB_BASE_URL = 'https://api.github.com/graphql';
-const GITHUB_PERSONAL_ACCESS_TOKEN = "cef2ef2cf27c4f57ec51153dd374e0226fc9a822";
+const GITHUB_PERSONAL_ACCESS_TOKEN = "281192404f2d44f42c8177ea5f28b3f9077a7348";
 
 const httpLink = new HttpLink({
   uri: GITHUB_BASE_URL,
